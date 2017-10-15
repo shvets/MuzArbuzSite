6411aa071c862f73f38ab7c033f702b33c16da5e
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-15 15:56:36 -0400

    rearrange code; reduce dependency on service adapter

--- Sources/MuzArbuzServiceAdapter.swift
+++ Sources/MuzArbuzServiceAdapter.swift
@@ -54,18 +54,18 @@ class MuzArbuzServiceAdapter: ServiceAdapter {
     history.add(item: item)
   }
 
-func getConfiguration() -> Configuration {
-    var conf = Configuration()
-
+  func getConfiguration() -> [String: Any] {
     if mobile {
-      conf.pageSize = 12
-      conf.rowSize = 1
+      return [
+        "pageSize": 12,
+        "rowSize": 1
+      ]
     }
     else {
-      conf.pageSize = 12
-      conf.rowSize = 6
+      return [
+        "pageSize": 12,
+        "rowSize": 6
+      ]
     }
-
-    return conf
   }
 }
