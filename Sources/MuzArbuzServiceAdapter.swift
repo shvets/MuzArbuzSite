9a73cfd12edf764422587aa2b0cf8ce567e7832e
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-15 21:21:46 -0400

    rearrange code; reduce dependency on service adapter

--- Sources/MuzArbuzServiceAdapter.swift
+++ Sources/MuzArbuzServiceAdapter.swift
@@ -10,14 +10,20 @@ class MuzArbuzServiceAdapter: ServiceAdapter {
   override open class var StoryboardId: String { return "MuzArbuz" }
   override open class var BundleId: String { return "com.rubikon.MuzArbuzSite" }
 
-    lazy var bookmarks = Bookmarks(MuzArbuzServiceAdapter.bookmarksFileName)
-    lazy var history = History(MuzArbuzServiceAdapter.historyFileName)
+  lazy var bookmarks = Bookmarks(MuzArbuzServiceAdapter.bookmarksFileName)
+  lazy var history = History(MuzArbuzServiceAdapter.historyFileName)
+
+  var bookmarksManager: BookmarksManager?
+  var historyManager: HistoryManager?
 
   public init(mobile: Bool=false) {
     super.init(dataSource: MuzArbuzDataSource(), mobile: mobile)
 
     bookmarks.load()
     history.load()
+
+    bookmarksManager = BookmarksManager(bookmarks)
+    historyManager = HistoryManager(history)
 
     pageLoader.pageSize = 12
     pageLoader.rowSize = 6
@@ -55,17 +61,20 @@ class MuzArbuzServiceAdapter: ServiceAdapter {
   }
 
   func getConfiguration() -> [String: Any] {
+    var conf = [String: Any]()
+
+    conf["pageSize"] = 12
+
     if mobile {
-      return [
-        "pageSize": 12,
-        "rowSize": 1
-      ]
+      conf["rowSize"] = 1
     }
     else {
-      return [
-        "pageSize": 12,
-        "rowSize": 6
-      ]
+      conf["rowSize"] = 6
     }
+
+    conf["bookmarksManager"] = bookmarksManager
+    conf["historyManager"] = historyManager
+
+    return conf
   }
 }
